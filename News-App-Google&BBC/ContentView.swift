5a9bf7634099d6c3278eb7c3dd2928615445cd63
rasyidrw <rasyidrespati.30@gmail.com>
2020-06-25 19:12:01 +0700

    Initial Commit

---
+++ News-App-Google&BBC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News-App-Google&BBC
+//
+//  Created by Rasyid Respati Wiriaatmaja on 25/06/20.
+//  Copyright © 2020 rasyidrw. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
